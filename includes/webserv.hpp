cc046b7ec4be62df0ae8f4d6c0d4f7caa9b122e3
HoussamMrabet <dodslim100@gmail.com>
2025-01-19 15:51:05 +0100

    init project

---
+++ includes/webserv.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   webserv.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmrabet <hmrabet@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/19 15:13:55 by hmrabet           #+#    #+#             */
+/*   Updated: 2025/01/19 15:15:53 by hmrabet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include "server.hpp"
+#include "request.hpp"
+#include "response.hpp"
+#include "cgi.hpp"
